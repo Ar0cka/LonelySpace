b281094bb0015b2a27b0b6424d7c0bf3a85911be
Ar0cka <120028878+Ar0cka@users.noreply.github.com>
2023-10-06 03:37:18 +1200

    Remove inventory scripts
    Удаление скрипта для инвентаря, для дальнейшего его изменения под новую систему.

--- LonelySpace3D/Assets/Scripts/Inventory/Abstract/IInvetoryItemState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IInventoryItemState 
-{
-    int amount { get; set; }
-    bool isEquipped { get; set; }  
-}
